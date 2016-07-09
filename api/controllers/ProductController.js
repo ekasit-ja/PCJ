e889fb08e5f8b4a6a51d0906d0a720e8085d9ec8
Ekasit Ja <ekasit.ja@gmail.com>
2016-07-09 11:53:13 +0700

    save (fire steel door page)

---
+++ api/controllers/ProductController.js
@@ -0,0 +1,13 @@
+/**
+ * ProductController
+ *
+ * @description :: Server-side logic for managing products
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	fireSteelDoor: function(req, res) {
+        res.view("product/fire_steel_door");
+    },
+};
+
