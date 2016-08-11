b3c81f092026407ead59802356b62fe457cb99d2
Ekasit Ja <ekasit.ja@gmail.com>
2016-08-11 11:44:04 +0700

    update fire steel door data

--- config/enums.js
+++ config/enums.js
@@ -7,12 +7,16 @@ module.exports.enums = {
     },
 
     hardware: {
-        dc: "door-closers",
+        fed: "fire-exit-devices",
         dl: "door-locks",
+        dhi: "door-hinge",
+        dc: "door-closers",
+        dho: "door-holders",
         ws: "wheel-set",
         dha: "door-handles",
-        dho: "door-holders",
-        fed: "fire-exit-devices",
+        seal: "seal",
+        adb: "automatic-door-bottom",
+        fdc: "fire-door-coordinator",
         etc: "etc",
     },
 
