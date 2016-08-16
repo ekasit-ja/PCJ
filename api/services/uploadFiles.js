01337ad80d2b291996fa02eb26f6708f4a0daabf
Ekasit Ja <ekasit.ja@gmail.com>
2016-08-16 12:53:06 +0700

    copy uploaded files to .tmp manually since production server does not do it automatically

--- api/services/uploadFiles.js
+++ api/services/uploadFiles.js
@@ -14,6 +14,19 @@ module.exports = function(file) {
                 };
             }
 
+
+            var fs = sails.fs;
+            for(var i=0; i<files.length; i++) {
+                var rd = fs.createReadStream(sails.prefixDir + files[i].extra.uploadPath);
+                rd.on("error", function(err) {});
+
+                var wr = fs.createWriteStream(".tmp/public/images/upload/" + files[i].extra.filename);
+                wr.on("error", function(err) {});
+                wr.on("close", function(ex) {});
+
+                rd.pipe(wr);
+            }
+
             return resolve(files);
         });
     });
