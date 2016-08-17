945a8b271d435741a11a2781ee02e894d7e37168
Ekasit Ja <ekasit.ja@gmail.com>
2016-08-17 11:03:08 +0700

    change back to user mysql

--- config/env/development.js
+++ config/env/development.js
@@ -10,27 +10,27 @@
  *
  */
 
-// var MySQLSessionStore = require('express-mysql-session');
-
-// var db_options = {
-//     adapter: 'sails-mysql',
-//     host: 'localhost',
-//     port: 3306,
-//     user: 'root',
-//     password: '1234',
-//     database: 'pcj_industries'
-// }
+var MySQLSessionStore = require('express-mysql-session');
 
 var db_options = {
-    adapter: 'sails-postgresql',
-    database: 'd82rbqaakir91u',
-    host: 'ec2-54-243-249-65.compute-1.amazonaws.com',
-    user: 'dkjpcoqvadpgup',
-    password: 'fJgjmGXEhj5J21DQQn9tZ5HTQr',
-    port: 5432,
-    poolSize: 10,
-    ssl: true,
+    adapter: 'sails-mysql',
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '1234',
+    database: 'pcj_industries'
 }
+
+// var db_options = {
+//     adapter: 'sails-postgresql',
+//     database: 'd82rbqaakir91u',
+//     host: 'ec2-54-243-249-65.compute-1.amazonaws.com',
+//     user: 'dkjpcoqvadpgup',
+//     password: 'fJgjmGXEhj5J21DQQn9tZ5HTQr',
+//     port: 5432,
+//     poolSize: 10,
+//     ssl: true,
+// }
 
 module.exports = {
 
@@ -48,9 +48,9 @@ module.exports = {
         migrate: 'safe'
     },
 
-    // session: {
-    //     store: new MySQLSessionStore(db_options),
-    // },
+    session: {
+        store: new MySQLSessionStore(db_options),
+    },
 
     hookTimeout: 60000,
 
