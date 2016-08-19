d6b0a381f456673bfc9a540614e9e36ea3dccbc0
Ekasit Ja <ekasit.ja@gmail.com>
2016-08-19 14:37:01 +0700

    update image_th of hardware

--- api/services/dynamicInter.js
+++ api/services/dynamicInter.js
@@ -2,22 +2,32 @@ module.exports = function(req, model, items) {
     if(req.getLocale() == "th") {
         if(Array.isArray(items)) {
             for(var i=0; i<items.length; i++) {
-                if(model == "Type" || model == "Product")
+                if(model == "Type" || model == "Product") {
                     items[i].title = items[i].title_th || items[i].title;
-
-                if(model == "Model" || model == "File" || model == "Hardware" || model == "Project") {
+                }
+                else if(model == "Model" || model == "File" || model == "Project") {
                     items[i].title = items[i].title_th || items[i].title;
                     items[i].desc = items[i].desc_th || items[i].desc;
+                }
+                else if(model == "Hardware") {
+                    items[i].title = items[i].title_th || items[i].title;
+                    items[i].desc = items[i].desc_th || items[i].desc;
+                    items[i].image = items[i].image_th || items[i].image;
                 }
             }
         }
         else if(typeof items != "undefined" && items != null) {
-            if(model == "Type" || model == "Product")
+            if(model == "Type" || model == "Product") {
                 items.title = items.title_th || items.title;
-
-            if(model == "Model" || model == "File" || model == "Hardware" || model == "Project") {
+            }
+            else if(model == "Model" || model == "File" || model == "Project") {
                 items.title = items.title_th || items.title;
                 items.desc = items.desc_th || items.desc;
+            }
+            else if(model == "Hardware") {
+                items.title = items.title_th || items.title;
+                items.desc = items.desc_th || items.desc;
+                items.image = items.image_th || items.image;
             }
         }
     }
