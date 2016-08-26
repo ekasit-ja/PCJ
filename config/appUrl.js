501928e2a6eedd291fa93ec857e560d672ede3a4
Ekasit Ja <ekasit.ja@gmail.com>
2016-08-26 11:21:15 +0700

    add link to social media

--- config/appUrl.js
+++ config/appUrl.js
@@ -1,3 +1,5 @@
 module.exports.appUrl = {
     base: "http://ekasit.cloudapp.net",
+    subFb: "https://www.facebook.com/pcjindustries/",
+    subYoutube: "https://www.youtube.com/watch?v=yF7bEn0LV10",
 }
