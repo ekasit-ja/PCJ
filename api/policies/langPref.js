b3cfad103b2c63b04cfd69a5e755ba44b9e8a17f
Ekasit Ja <ekasit.ja@gmail.com>
2016-08-27 11:35:07 +0700

    make default lang to EN

--- api/policies/langPref.js
+++ api/policies/langPref.js
@@ -1,6 +1,8 @@
 module.exports = function(req, res, next) {
     if(req.session.langPref)
         req.setLocale(req.session.langPref);
+    else
+        req.setLocale(sails.config.i18n.defaultLocale);
 
     return next();
 };
