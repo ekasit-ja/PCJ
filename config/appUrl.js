1ff1877a3425f11c5cf6f34e6f72bf71b52f9467
Ekasit Ja <ekasit.ja@gmail.com>
2016-09-04 15:27:23 +0700

    update base url

--- config/appUrl.js
+++ config/appUrl.js
@@ -1,5 +1,5 @@
 module.exports.appUrl = {
-    base: "http://ekasit.cloudapp.net",
+    base: "http://www.pcjindustries.co.th",
     subFb: "https://www.facebook.com/pcjindustries/",
     subYoutube: "https://www.youtube.com/channel/UCGl6i4eh4_ctQNTyeiIPssQ",
 }
