ff55781c4bf054c48d03e91e1cd8118d8ff8ecde
Ekasit Ja <ekasit.ja@gmail.com>
2016-09-10 23:59:53 +0700

    subdomain tolowercase

--- api/policies/langPref.js
+++ api/policies/langPref.js
@@ -1,7 +1,7 @@
 module.exports = function(req, res, next) {
     var sub = req.subdomains;
     for(var i=0; i<sub.length; i++)
-        if(sub[i] == "mail")
+        if(sub[i].toLowerCase() == "mail" || sub[i].toLowerCase() == "webmail")
             return res.redirect(sails.config.appUrl.webmail);
 
     if(req.session.langPref)
