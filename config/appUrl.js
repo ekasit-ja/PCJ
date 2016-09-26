db3bf4d920aca2e8c6e2b25e0b22aaf7c2323144
Ekasit Ja <ekasit.ja@gmail.com>
2016-09-26 08:36:07 +0700

    update app url

--- config/appUrl.js
+++ config/appUrl.js
@@ -1,7 +1,7 @@
 module.exports.appUrl = {
     base: "http://www.pcjindustries.co.th",
     webmail: "http://www.pcjindustries.co.th:2095/",
-    whm: "http://www.pcjindustries.co.th:2087/",
+    whm: "https://server.pcjindustries.co.th:2087/",
     subFb: "https://www.facebook.com/pcjindustries/",
     subYoutube: "https://www.youtube.com/channel/UCGl6i4eh4_ctQNTyeiIPssQ",
 }
