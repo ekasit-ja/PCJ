a25be9d39acbc684be15252f755db896ac6cec55
Ekasit Ja <ekasit.ja@gmail.com>
2018-06-05 22:16:15 +0700

    upgrade redirect

--- config/appUrl.js
+++ config/appUrl.js
@@ -1,6 +1,6 @@
 module.exports.appUrl = {
-    base: "http://www.pcjindustries.co.th",
-    webmail: "http://www.pcjindustries.co.th:2095/",
+    base: "https://www.pcjindustries.co.th",
+    webmail: "https://www.pcjindustries.co.th:2095/",
     whm: "https://server.pcjindustries.co.th:2087/",
     subFb: "https://www.facebook.com/pcjindustries/",
     subYoutube: "https://www.youtube.com/channel/UCGl6i4eh4_ctQNTyeiIPssQ",
